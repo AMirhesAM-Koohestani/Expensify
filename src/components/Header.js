f05f9bb956e3b93090a0743fc43e1eaba316f1ee
Alex .M <amirhesam.kouhestani@gmail.com>
2020-05-11 02:27:54 -0700

    Add Expensify title in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 const Header = () => (
     <header>
+        <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact={true} >Home</NavLink>
         <NavLink to="/create" activeClassName="is-active" >New Expense</NavLink>
     </header>
