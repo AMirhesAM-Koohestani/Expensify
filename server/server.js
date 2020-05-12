cc0c055261325cb892c8266561c1b4c6b4f4ac2f
Alex .M <amirhesam.kouhestani@gmail.com>
2020-05-12 02:05:36 -0700

    Setup express server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const publicDirectory = path.join(__dirname, '..', 'public')
+const port = 3000
+
+app.use(express.static(publicDirectory))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicDirectory, 'index.html'))
+})
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
+})
