5bd7db916a44124221ed9f4c55cbdb143cd199be
Alex .M <amirhesam.kouhestani@gmail.com>
2020-05-13 19:40:11 -0700

    Setup final production build and server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const app = express()
 const publicDirectory = path.join(__dirname, '..', 'public')
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static(publicDirectory))
 
